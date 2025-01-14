3ef715a78c7ec8446166b94c003cecc1ff6ddc4a
Alyona <akovalcuk485@gmail.com>
2025-01-14 12:01:37 +0200

    finish markup

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+const initMongoConnection = async () => {
+  dotenv.config();
+  const MONGODB_USER = process.env.MONGODB_USER;
+  const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+  const MONGODB_URL = process.env.MONGODB_URL;
+  const MONGODB_DB = process.env.MONGODB_DB;
+  try {
+    await mongoose.connect(
+      `mongodb+srv://akovalcuk485:<db_password>@cluster0.xl23b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    );
+    console.log('Mongo connection successfully connected!');
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export default initMongoConnection;
