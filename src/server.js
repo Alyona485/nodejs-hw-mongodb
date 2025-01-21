3cac0b3ba2c80bfae369d4bae13b0299182c63fd
Alyona <akovalcuk485@gmail.com>
2025-01-21 12:17:13 +0200

    finish markup

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-import dotenv from 'dotenv';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
+// імпорт мідлварів
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
+// далі встановлюю порт
+const PORT = process.env.PORT || 3000;
 
-const setupServer = () => {
+export function setupServer() {
+  // створюю екземпляр сервера
   const app = express();
-  dotenv.config();
-  const PORT = Number(process.env.PORT);
 
+  // парсю
+  app.use(express.json());
+
+  // далі додаю мідлвари (середовище)
   app.use(cors());
 
   app.use(
@@ -17,54 +24,17 @@ const setupServer = () => {
       transport: {
         target: 'pino-pretty',
       },
-    })
+    }),
   );
 
-  app.get('/contacts', async (req, res, next) => {
-    try {
-      const contacts = await getAllContacts();
-      res.json({
-        status: 200,
-        message: 'Successfully found contacts!',
-        data: contacts,
-      });
-    } catch (error) {
-      next(error);
-    }
-  });
+  app.use(contactsRouter);
 
-  app.get('/contacts/:id', async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const contact = await getContactById(id);
-      if (!contact) {
-        return res.status(404).json({ status: 404,
-          message: `Contact not found`, });
-      }
-      res.json({
-        status: 200,
-        message: `Successfully found contact with id ${id}!`,
-        data: contact,
-      });
-    } catch (error) {
-      next(error);
-    }
-  });
+  app.use('*', notFoundHandler);
 
-  app.use((err, req, res, next) => {
-    const { status = 500, message = 'server error' } = err;
-    res.status(status).json({ message });
-  });
+  app.use(errorHandler);
 
-  app.use((req, res) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
+  //   і запускаю сервер
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-};
-
-export default setupServer;
+}
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,11 +1,55 @@
-import ContactsCollection from '../db/models/contacts.js';
+import { ContactsCollection } from '../db/models/contacts.js';
 
 export const getAllContacts = async () => {
-  const contacts = await ContactsCollection.find();
-  return contacts;
+  try {
+    console.log('Starting getAllContacts...');
+    const contacts = await ContactsCollection.find();
+    console.log('Fetched contacts:', contacts);
+    return contacts;
+  } catch (error) {
+    console.error('Error fetching contacts:', error);
+    throw error;
+  }
 };
 
-export const getContactById = async contactId => {
-  const contact = await ContactsCollection.findById(contactId);
+export const getContactById = async (contactId) => {
+  try {
+    const contact = await ContactsCollection.findById(contactId);
+    return contact;
+  } catch (error) {
+    console.error('Error fetching contacts:', error);
+    throw error;
+  }
+};
+
+export const createContact = async (payload) => {
+  const contact = await ContactsCollection.create(payload);
+  return contact;
+};
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const rawResult = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    student: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = await ContactsCollection.findOneAndDelete({
+    _id: contactId,
+  });
+
   return contact;
 };
