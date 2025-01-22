704be547e554ebfca28397e3e0a357e869d72e77
Alyona <akovalcuk485@gmail.com>
2025-01-22 11:19:00 +0200

    finish markup

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,17 @@
+import { isHttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
-    res.status(500).json({
-      status: 500,
-      message: 'Something went wrong',
-      data: err.message,
+  if (isHttpError(err)) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
     });
-  };
+    return;
+  }
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
+    data: err.message,
+  });
+};
